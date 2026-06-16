933d0694ff70420b36113f4f9c9bf368df1d6b67
firetdev <futuregamesstore@gmail.com>
2026-06-15 23:30:22 -0500

    Initial Commit

---
+++ synth_test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  synth_test
+//
+//  Created by Yeshaya Zeff on 6/15/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
